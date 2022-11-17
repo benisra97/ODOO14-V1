fe0a447cf8b01f70b600f97c2ea6cf78163e09fc
benisra97 <111304597+benisra97@users.noreply.github.com>
2022-11-17 11:14:19 -0600

    Add files via upload

---
+++ custom/addons/primer_modulo/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Mi modulo",
+    'summary': """"
+                curso
+    """,
+    'author':'Israel',
+    'category':'general',
+    'version': '1.0.0',
+    'depends': ['sale',],
+    'data': [
+        'views/project.xml',
+        'views/sale.xml',
+        'views/report.xml',
+        'views/report_view.xml',
+        'views/menu_view.xml',
+
+    ],
+
+
+}
